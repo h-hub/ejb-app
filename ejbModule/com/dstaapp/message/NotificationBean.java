fcfd506e767a66585d1553ba039609e403c51092
h-hub <harsha.kj89@gmail.com>
2018-01-01 23:34:56 +0800

    notofocation changes

--- ejbModule/com/dstaapp/message/NotificationBean.java
+++ ejbModule/com/dstaapp/message/NotificationBean.java
@@ -2,33 +2,52 @@ package com.dstaapp.message;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+import com.dstaapp.model.User;
 
 /**
  * Message-Driven Bean implementation class for: NotificationBean
  */
-@MessageDriven(
-		activationConfig = { @ActivationConfigProperty(
-				propertyName = "destination", propertyValue = "NotificationQueue"), @ActivationConfigProperty(
-				propertyName = "destinationType", propertyValue = "javax.jms.Queue")
-		}, 
-		mappedName = "NotificationQueue")
+@MessageDriven(activationConfig = {
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "NotificationQueue"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue") }, mappedName = "NotificationQueue")
 public class NotificationBean implements MessageListener {
 
-    /**
-     * Default constructor. 
-     */
-    public NotificationBean() {
-        // TODO Auto-generated constructor stub
-    }
-	
 	/**
-     * @see MessageListener#onMessage(Message)
-     */
-    public void onMessage(Message message) {
-        // TODO Auto-generated method stub
-        
-    }
+	 * Default constructor.
+	 */
+	public NotificationBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see MessageListener#onMessage(Message)
+	 */
+	public void onMessage(Message message) {
+		try {
+			Object msgObj = ((ObjectMessage) message).getObject();
+			if (msgObj != null) {
+				User user = (User) msgObj;
+				System.out.println("Customer with the following details has been updated:");
+				StringBuilder sb = new StringBuilder();
+				sb.append("Customer ID=");
+				sb.append(user.getId());
+				sb.append(", ");
+				sb.append("Name=");
+				sb.append(user.getUsername());
+				sb.append(", ");
+				sb.append("Email=");
+				sb.append(user.getEmail());
+				System.out.println(sb.toString());
+			}
+		} catch (JMSException ex) {
+			//Logger.getLogger(NotificationBean.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+	}
 
 }
--- ejbModule/com/dstaapp/session/MangeUserSessionBean.java
+++ ejbModule/com/dstaapp/session/MangeUserSessionBean.java
@@ -1,11 +1,21 @@
 package com.dstaapp.session;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.annotation.Resource;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Queue;
+import javax.jms.Session;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -22,6 +32,12 @@ public class MangeUserSessionBean implements MangeUserSessionBeanLocal {
 
 	@PersistenceContext
 	private EntityManager entityManager;
+
+	@Resource(name = "jms/queue/notification")
+	private Queue notificationQueue;
+
+	@Resource(name = "java:jboss/exported/jms/RemoteConnectionFactory")
+	private ConnectionFactory notificationQueueFactory;
 
 	/**
 	 * Default constructor.
@@ -71,8 +87,48 @@ public class MangeUserSessionBean implements MangeUserSessionBeanLocal {
 		Set<Role> roles = new HashSet<Role>(userRole);
 
 		newUser.setRoles(roles);
+
 		entityManager.persist(newUser);
+
+		try {
+			sendJMSMessageToNotificationQueue(newUser);
+		} catch (JMSException ex) {
+			// Logger.getLogger(CustomerSessionBean.class.getName()).log(Level.SEVERE, null,
+			// ex);
+		}
+
 		return true;
 	}
 
+	private Message createJMSMessageForjmsNotificationQueue(Session session, Object messageData) throws JMSException {
+		// Modified to use ObjectMessage instead
+		ObjectMessage tm = session.createObjectMessage();
+		tm.setObject((Serializable) messageData);
+		return tm;
+	}
+
+	private void sendJMSMessageToNotificationQueue(Object messageData) throws JMSException {
+		Connection connection = null;
+		Session session = null;
+		try {
+			connection = notificationQueueFactory.createConnection();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			MessageProducer messageProducer = session.createProducer(notificationQueue);
+			messageProducer.send(createJMSMessageForjmsNotificationQueue(session, messageData));
+		} finally {
+			if (session != null) {
+				try {
+					session.close();
+
+				} catch (JMSException e) {
+					// Logger.getLogger(this.getClass().getName()).log(Level.WARNING, "Cannot close
+					// session", e);
+				}
+			}
+			if (connection != null) {
+				connection.close();
+			}
+		}
+
+	}
 }
